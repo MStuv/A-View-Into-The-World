1049243858db1d3442de9d327249868494245ab2
MStuv <markstuver@MacBook-Pro.local>
2013-11-29 08:08:22 -0500

    Initial Commit

---
+++ A View Into The World/MASAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MASAppDelegate.h
+//  A View Into The World
+//
+//  Created by Mark Stuver on 11/29/13.
+//  Copyright (c) 2013 Halo International Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MASAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
